fd0e9c19c14766d2c8b9ee3dfd9b6baa8e06e8e0
Rafael Leite <r.cmgleite@gmail.com>
2024-05-19 16:30:42 -0700

    Rework the Db struct
    just cleaning up...

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -9,9 +9,9 @@
 //!    - currently a simple header (cmd,length) and a json encoded payload
 use crate::cluster::heartbeat::start_heartbeat;
 use crate::cluster::ring_state::RingState;
+use crate::cmd::Command;
 use crate::error::{Error, Result};
 use crate::storage_engine::in_memory::InMemory;
-use crate::{cmd::Command, storage_engine::StorageEngine};
 use bytes::Bytes;
 use std::sync::Arc;
 use std::{fmt::Debug, path::PathBuf};
@@ -27,35 +27,29 @@ use self::message::Message;
 pub mod config;
 pub mod message;
 
-pub type SyncStorageEngine = Arc<dyn StorageEngine + Send + Sync + 'static>;
+pub type StorageEngine = Arc<dyn crate::storage_engine::StorageEngine + Send + Sync + 'static>;
 
 pub struct Server {
+    /// listener for incoming client requests
     client_listener: TcpListener,
+    /// listener for incoming cluster (gossip) requests
+    /// Only exists in cluster configuration
+    cluster_listener: Option<TcpListener>,
     db: Arc<Db>,
 }
 
-// TODO: This Db struct is very cumbersome... rework it..
 #[derive(Debug)]
 pub struct Db {
-    pub storage_engine: SyncStorageEngine,
+    /// the underlaying storage engine
+    pub storage_engine: StorageEngine,
+    /// the partition scheme (if any)
+    /// This will be present if this is configured as a cluster node
     pub partitioning_scheme: Option<Arc<PartitioningScheme>>,
-    pub state: ServerState,
 }
 
 #[derive(Debug)]
 pub enum PartitioningScheme {
     ConsistentHashing(RingState),
-}
-
-#[derive(Debug)]
-pub enum ServerMode {
-    Standalone,
-    Cluster { gossip_listener: TcpListener },
-}
-
-#[derive(Debug)]
-pub struct ServerState {
-    pub mode: ServerMode,
 }
 
 impl Server {
@@ -78,12 +72,10 @@ impl Server {
 
                 Ok(Self {
                     client_listener: listener,
+                    cluster_listener: None,
                     db: Arc::new(Db {
                         storage_engine,
                         partitioning_scheme: None,
-                        state: ServerState {
-                            mode: ServerMode::Standalone,
-                        },
                     }),
                 })
             }
@@ -113,7 +105,7 @@ impl Server {
                     }
                 };
 
-                let gossip_listener =
+                let cluster_listener =
                     TcpListener::bind(format!("127.0.0.1:{}", gossip.port)).await?;
 
                 // FIXME: There's a big problem here - if this task exists how will
@@ -122,12 +114,10 @@ impl Server {
 
                 Ok(Self {
                     client_listener,
+                    cluster_listener: Some(cluster_listener),
                     db: Arc::new(Db {
                         storage_engine,
                         partitioning_scheme: Some(partitioning_scheme),
-                        state: ServerState {
-                            mode: ServerMode::Cluster { gossip_listener },
-                        },
                     }),
                 })
             }
@@ -136,15 +126,15 @@ impl Server {
 
     pub async fn run(&mut self) -> Result<()> {
         event!(Level::INFO, "Listener started");
-        if let ServerMode::Cluster { gossip_listener } = &self.db.state.mode {
-            // for now let's accept new connections from both the client_listener and gossip_listener
-            // in the same server instance.. this might become very odd very quickly
+
+        // if we have a cluster listener, accept connection from both client and cluster listeners.
+        if let Some(cluster_listener) = self.cluster_listener.take() {
             loop {
                 let conn = tokio::select! {
                     Ok((conn, _)) = self.client_listener.accept() => {
                        conn
                     }
-                    Ok((conn, _)) = gossip_listener.accept() => {
+                    Ok((conn, _)) = cluster_listener.accept() => {
                         conn
                     }
                 };
@@ -153,6 +143,7 @@ impl Server {
                 tokio::spawn(handle_connection(conn, db));
             }
         } else {
+            // otherwise only listen to client connections
             loop {
                 let (conn, _) = self.client_listener.accept().await?;
                 let db = self.db.clone();
